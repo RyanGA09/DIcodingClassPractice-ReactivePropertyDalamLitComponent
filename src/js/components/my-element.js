7d4964e5da5b2931f5c8c1a1ab2d325020e4a08f
Ryan <ryangadinga90@gmail.com>
2025-07-27 09:11:07 +0700

    update JS & HTML

--- src/js/components/my-element.js
+++ src/js/components/my-element.js
@@ -1,5 +1,35 @@
-import { LitElement } from 'lit';
+import { html, LitElement } from 'lit';
 
-class MyElement extends LitElement {}
+class MyElement extends LitElement {
+  static properties = {
+    name: {},
+  };
+
+  render() {
+    let template;
+    if (this.name) {
+      template = html`
+        <p>Selamat datang, ${this.name}</p>
+        <p>
+          <button @click=${this._logout}>Keluar</button>
+        </p>
+      `;
+    } else {
+      template = html`
+        <p>Silakan masuk terlebih dulu:</p>
+        <p>
+          <button @click=${this._login}>Masuk</button>
+        </p>
+      `;
+    }
+    return template;
+  }
+  _login() {
+    this.name = 'John Doe';
+  }
+  _logout() {
+    this.name = null;
+  }
+}
 
 customElements.define('my-element', MyElement);
